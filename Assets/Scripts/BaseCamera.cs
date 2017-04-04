ba461e495164953162264cb0a85e5f2515fe946a
Chad McLeavy <chad_mcleavy@hotmail.com>
2017-04-03 22:17:25 -0700

    Layed out a tinny test level with player, zombie and three dungeon tiles.

---
+++ Assets/Scripts/BaseCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCamera : MonoBehaviour
+{
+    public float speed;
+
+    public Vector3 positionOffset;
+
+    private GameObject _target;
+
+    private void Start()
+    {
+        if (FindObjectOfType<Player>())
+            _target = FindObjectOfType<Player>().gameObject;
+    }
+    private void FixedUpdate()
+    {
+        moveCamera();
+    }
+
+    void moveCamera()
+    {
+        Vector3 targetPosition = _target.transform.position;
+
+        targetPosition = targetPosition + positionOffset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, speed * 0.01f);
+
+        transform.LookAt(_target.transform, Vector3.up);
+    }
+}
