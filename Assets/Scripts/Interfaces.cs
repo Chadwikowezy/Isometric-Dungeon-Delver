bf90c50f9b3d2dd759abf9b5d6d4f40f3e321798
Chad McLeavy <chad_mcleavy@hotmail.com>
2017-04-13 19:44:26 -0700

    Created wooden sword as a test item

--- Assets/Scripts/Interfaces.cs
+++ Assets/Scripts/Interfaces.cs
@@ -2,7 +2,26 @@
 using UnityEngine;
 
 public interface IEquipable
-{ void Equip(BaseUnit character); }
+{
+    int Health
+    { get; set; }
+    int Attack
+    { get; set; }
+    int Defense
+    { get; set; }
+
+    float Range
+    { get; set; }
+    float Knockback
+    { get; set; }
+    float Cooldown
+    { get; set; }
+    float StunTime
+    { get; set; }
+
+    void Equip(BaseUnit character);
+    void Unequip(BaseUnit character);
+}
 
 public interface IConsumable
 { void Consume(); }
--- Assets/Scripts/Items/WoodenSword.cs
+++ Assets/Scripts/Items/WoodenSword.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class WoodenSword : BaseItem, IEquipable
 {
+    //Properties
+    public int Health
+    { get; set; }
+    public int Attack
+    { get; set; }
+    public int Defense
+    { get; set; }
+    public float Range
+    { get; set; }
+    public float Knockback
+    { get; set; }
+    public float Cooldown
+    { get; set; }
+    public float StunTime
+    { get; set; }
+
     public void Equip(BaseUnit character)
+    {
+
+    }
+    public void Unequip(BaseUnit character)
     {
 
     }
